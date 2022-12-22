7ebb8b561b51267ddd592512b35e3c8d2307ee99
Srinivas R <srinivas@cognixsoft.com>
2022-12-22 10:52:56 +0530

    routing changes

---
+++ src/app/core/auth/guards/admin.guard.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { AuthService } from '../auth.service';
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const url = state.url;
+
+    if (this.authService.role !== 'superAdmin' || 'admin') {
+      if (this.authService.role === 'superAdmin') {
+        if (
+          url.match('/admin/dashboard') ||
+          url.match('/admin/companies/list') ||
+          url.match('/admin/integrations/list') ||
+          url.match('/admin/users/list') ||
+          url.match('/admin/sources/list')
+        ) {
+          return true;
+        }
+      }
+      if (this.authService.role === 'admin') {
+        if (
+          url.match('/admin/dashboard') ||
+          url.match('/admin/companies/list') ||
+          url.match('/admin/users/list')
+        ) {
+          return true;
+        }
+      }
+      this.router.navigate(['**']);
+      return false;
+    }
+  }
+}
