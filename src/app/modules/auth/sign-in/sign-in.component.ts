3dfc3d68fa0c5cb5d720ac59be1453e5366a25c4
Hitesh <hiteshwork13@gmail.com>
2023-02-08 17:10:10 +0530

    Note : 1. Overwrite scss variable 2. replace necessary css to apply perfect style in add-integration component

--- src/app/modules/auth/sign-in/sign-in.component.ts
+++ src/app/modules/auth/sign-in/sign-in.component.ts
@@ -1,114 +1,117 @@
 import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import {
-  UntypedFormBuilder,
-  UntypedFormGroup,
-  NgForm,
-  Validators,
+    UntypedFormBuilder,
+    UntypedFormGroup,
+    NgForm,
+    Validators,
 } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { fuseAnimations } from '@fuse/animations';
 import { FuseAlertType } from '@fuse/components/alert';
 import { AuthService } from 'app/core/auth/auth.service';
+import { UserService } from 'app/core/user/user.service';
 
 @Component({
-  selector: 'auth-sign-in',
-  templateUrl: './sign-in.component.html',
-  encapsulation: ViewEncapsulation.None,
-  animations: fuseAnimations,
+    selector: 'auth-sign-in',
+    templateUrl: './sign-in.component.html',
+    encapsulation: ViewEncapsulation.None,
+    animations: fuseAnimations,
 })
 export class AuthSignInComponent implements OnInit {
-  @ViewChild('signInNgForm') signInNgForm: NgForm;
+    @ViewChild('signInNgForm') signInNgForm: NgForm;
 
-  alert: { type: FuseAlertType; message: string } = {
-    type: 'success',
-    message: '',
-  };
-  signInForm: UntypedFormGroup;
-  showAlert: boolean = false;
+    alert: { type: FuseAlertType; message: string } = {
+        type: 'success',
+        message: '',
+    };
+    signInForm: UntypedFormGroup;
+    showAlert: boolean = false;
 
-  /**
-   * Constructor
-   */
-  constructor(
-    private _activatedRoute: ActivatedRoute,
-    private _authService: AuthService,
-    private _formBuilder: UntypedFormBuilder,
-    private _router: Router
-  ) {}
+    /**
+     * Constructor
+     */
+    constructor(
+        private _activatedRoute: ActivatedRoute,
+        private _authService: AuthService,
+        private _formBuilder: UntypedFormBuilder,
+        private _router: Router,
+        private _userService: UserService
+    ) { }
 
-  // -----------------------------------------------------------------------------------------------------
-  // @ Lifecycle hooks
-  // -----------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------
+    // @ Lifecycle hooks
+    // -----------------------------------------------------------------------------------------------------
 
-  /**
-   * On init
-   */
-  ngOnInit(): void {
-    // Create the form
-    this.signInForm = this._formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
-      rememberMe: [''],
-    });
-  }
-
-  // -----------------------------------------------------------------------------------------------------
-  // @ Public methods
-  // -----------------------------------------------------------------------------------------------------
-
-  /**
-   * Sign in
-   */
-  signIn(): void {
-    // Return if the form is invalid
-    if (this.signInForm.invalid) {
-      return;
+    /**
+     * On init
+     */
+    ngOnInit(): void {
+        this._userService.removeTheme();
+        // Create the form
+        this.signInForm = this._formBuilder.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', Validators.required],
+            rememberMe: [''],
+        });
     }
 
-    // Disable the form
-    this.signInForm.disable();
+    // -----------------------------------------------------------------------------------------------------
+    // @ Public methods
+    // -----------------------------------------------------------------------------------------------------
 
-    // Hide the alert
-    this.showAlert = false;
-
-    // Sign in
-    this._authService.signIn(this.signInForm.value).subscribe(
-      result => {
-        if (result.success === true) {
-          // Set the redirect url.
-          const redirectURL =
-            this._activatedRoute.snapshot.queryParamMap.get('redirectURL') ||
-            '/signed-in-redirect';
-         
-          // Navigate to the redirect url
-          this._router.navigateByUrl(redirectURL);
-        } else {
-          // Re-enable the form
-          this.signInForm.enable();
-          // Reset the form
-          this.signInNgForm.resetForm();
-          // Set the alert
-          this.alert = {
-            type: 'error',
-            message: 'Wrong email or password',
-          };
-          // Show the alert
-          this.showAlert = true;
+    /**
+     * Sign in
+     */
+    signIn(): void {
+        // Return if the form is invalid
+        if (this.signInForm.invalid) {
+            return;
         }
-      },
-      error => {
-        // Re-enable the form
-        this.signInForm.enable();
-        // Reset the form
-        this.signInNgForm.resetForm();
-        // Set the alert
-        this.alert = {
-          type: 'error',
-          message: 'Wrong email or password',
-        };
-        // Show the alert
-        this.showAlert = true;
-      }
-    );
-  }
+
+        // Disable the form
+        this.signInForm.disable();
+
+        // Hide the alert
+        this.showAlert = false;
+
+        // Sign in
+        this._authService.signIn(this.signInForm.value).subscribe(
+            result => {
+                if (result.success === true) {
+                    // Set the redirect url.
+                    const redirectURL =
+                        this._activatedRoute.snapshot.queryParamMap.get('redirectURL') ||
+                        '/signed-in-redirect';
+
+                    // Navigate to the redirect url
+                    this._router.navigateByUrl(redirectURL);
+                } else {
+                    // Re-enable the form
+                    this.signInForm.enable();
+                    // Reset the form
+                    this.signInNgForm.resetForm();
+                    // Set the alert
+                    this.alert = {
+                        type: 'error',
+                        message: 'Wrong email or password',
+                    };
+                    // Show the alert
+                    this.showAlert = true;
+                }
+            },
+            error => {
+                // Re-enable the form
+                this.signInForm.enable();
+                // Reset the form
+                this.signInNgForm.resetForm();
+                // Set the alert
+                this.alert = {
+                    type: 'error',
+                    message: 'Wrong email or password',
+                };
+                // Show the alert
+                this.showAlert = true;
+            }
+        );
+    }
 }
